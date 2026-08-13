caafcb2a6a89c01c25be585bc537128fc6d218b3
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-13 22:52:55 +0900

    State を２つに分ける

--- backend/internal/event/model.go
+++ backend/internal/event/model.go
@@ -6,11 +6,24 @@ import (
 	"github.com/naoto-anzai/quizApp/backend/internal/question"
 )
 
-type State struct{
+// DB に保存するための構造体
+type EventState struct{
 	Phase string `json:"phase"`
 	RemainingPlayers *int `json:"remainingPlayers"`
 	TimeLimitSec int `json:"timeLimitSec"`
 	QuestionStartedAt *time.Time `json:"questionStartedAt"`
 	RevealedSegments int `json:"revealedSegments"`
+	Question *question.Question `json:"question"`
+}
+
+// API で返すための構造体
+type State struct{
+	Phase string `json:"phase"`
+	ServerTime time.Time `json:"serverTime"`
+	RemainingPlayers *int `json:"remainingPlayers"`
+	TimeLimitSec int `json:"timeLimitSec"`
+	QuestionStartedAt *time.Time `json:"questionStartedAt"`
+	RevealedSegments int `json:"revealedSegments"`
+	TotalSegments int `json:"totalSegments"`
 	Question *question.Question `json:"question"`
 }
