efeabb8e961a71ae151025eba844a092ea5ebf81
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-13 22:30:10 +0900

    Sate型から不要なfield の削除

--- backend/internal/event/model.go
+++ backend/internal/event/model.go
@@ -2,11 +2,9 @@ package event
 
 type State struct{
 	Phase string `json:"phase"`
-	ServerTime string `json:"serverTime"`
 	RemainingPlayers int `json:"remainingPlayers"`
 	TimeLimitSec int `json:"timeLimitSec"`
 	QuestionStartedAt *string `json:"questionStartedAt"`
 	RevealdSegments int `json:"revealdSegments"`
-	TotalSegments int `json:"totalSegments"`
 	Question *Question `json:"question"`
 }
