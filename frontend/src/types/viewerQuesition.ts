e3b5e954a57cd78ab61bbf512ba06cb5e83a1861
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-16 14:02:18 +0900

    スマホ画面表示用のQuestion型を作成

---
+++ frontend/src/types/viewerQuesition.ts
@@ -0,0 +1,10 @@
+import { Choice } from "./choice"
+import { QuestionType } from "./questionType"
+
+export type ViewerQuestion  = {
+  number: number,
+  type: QuestionType,
+  textSegments: string[],
+  imageUrl: string | null,
+  choices: Choice[],
+}
