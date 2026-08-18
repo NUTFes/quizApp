bb7bbcf687ab0ce4d7a0974405ec6b66f95c4e71
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-19 01:33:49 +0900

    モックデータを引数としてそれぞれの関数で渡す

--- frontend/src/lib/useEventState.ts
+++ frontend/src/lib/useEventState.ts
@@ -1,10 +1,22 @@
 import { useEffect, useState } from "react"
 import type { AdminState, MonitorState, ViewerState } from "../types"
+import { adminWaiting } from "./mock/admin/adminWaiting"
+import { adminQuestionFour, adminQuestionArunashi } from "./mock/admin/adminQuestion"
+import { adminAnswerAri, adminAnswerNashi } from "./mock/admin/adminAnswer"
+import { adminFinished } from "./mock/admin/adminFinished"
+import { monitorWaiting } from "./mock/monitor/monitorWaiting"
+import { monitorQuestionFour, monitorQuestionArunashi } from "./mock/monitor/monitorQuestion"
+import { monitorAnswerAri, monitorAnswerNashi } from "./mock/monitor/monitorAnswer"
+import { monitorFinished } from "./mock/monitor/monitorFinished"
+import { phoneWaiting } from "./mock/phone/phoneWaiting"
+import { phoneQuestionFour, phoneQuestionArunashi } from "./mock/phone/phoneQuestion"
+import { phoneAnswerAri, phoneAnswerNashi } from "./mock/phone/phoneAnswer"
+import { phoneFinished } from "./mock/phone/phoneFinished"
 
 function useEventState<Type>(testSteps: {at:number, mock:Type}[]) :Type | null{
     const [state, setState] = useState<Type | null>(null)
-    const ts:ReturnType<typeof setTimeout>[] = []
     useEffect(() =>{
+        const ts:ReturnType<typeof setTimeout>[] = []
         for(const {at, mock} of testSteps){
             ts.push(setTimeout(() => setState(mock), at))
         }
@@ -19,6 +31,27 @@ function useEventState<Type>(testSteps: {at:number, mock:Type}[]) :Type | null{
     return state
 }
 
-export const useAdminState = () => useEventState<AdminState>()
-export const useMonitorState = () => useEventState<MonitorState>()
-export const useViewerState = () => useEventState<ViewerState>()
+export const useAdminState = () => useEventState<AdminState>([
+    { at:   300, mock: adminWaiting          },
+    { at:  2000, mock: adminQuestionFour     },
+    { at:  5000, mock: adminAnswerAri        },
+    { at:  8000, mock: adminQuestionArunashi },
+    { at: 11000, mock: adminAnswerNashi      },
+    { at: 14000, mock: adminFinished         },
+])
+export const useMonitorState = () => useEventState<MonitorState>([
+    { at:   300, mock: monitorWaiting          },
+    { at:  2000, mock: monitorQuestionFour     },
+    { at:  5000, mock: monitorAnswerAri        },
+    { at:  8000, mock: monitorQuestionArunashi },
+    { at: 11000, mock: monitorAnswerNashi      },
+    { at: 14000, mock: monitorFinished         },
+])
+export const useViewerState = () => useEventState<ViewerState>([
+    { at:   300, mock: phoneWaiting          },
+    { at:  2000, mock: phoneQuestionFour     },
+    { at:  5000, mock: phoneAnswerAri        },
+    { at:  8000, mock: phoneQuestionArunashi },
+    { at: 11000, mock: phoneAnswerNashi      },
+    { at: 14000, mock: phoneFinished         },
+])
