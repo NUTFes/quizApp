7e933f6a235dcb3c98b37d8f517b79c25e91ddb8
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-19 04:18:32 +0900

    モックデータのエクスポートを改善

---
+++ frontend/src/lib/mock/monitor/index.ts
@@ -0,0 +1,4 @@
+export * from './monitorAnswer'
+export * from './monitorFinished'
+export * from './monitorQuestion'
+export * from './monitorWaiting'
---
+++ frontend/src/lib/mock/phone/index.ts
@@ -0,0 +1,4 @@
+export * from './phoneAnswer'
+export * from './phoneFinished'
+export * from './phoneQuestion'
+export * from './phoneWaiting'
