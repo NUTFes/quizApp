c7efd3c9fec5ce62f9e2e8bbf147129874df1fa3
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-19 01:22:40 +0900

    モックデータに要る検証のためのEventState実装

--- frontend/src/lib/useEventState.ts
+++ frontend/src/lib/useEventState.ts
@@ -1,8 +1,21 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import type { AdminState, MonitorState, ViewerState } from "../types"
 
-function useEventState<Type>() :Type | null{
+function useEventState<Type>(testSteps: {at:number, mock:Type}[]) :Type | null{
     const [state, setState] = useState<Type | null>(null)
+    const ts:ReturnType<typeof setTimeout>[] = []
+    useEffect(() =>{
+        for(const {at, mock} of testSteps){
+            ts.push(setTimeout(() => setState(mock), at))
+        }
+        return () => {
+            console.log("in useEffect.return() 接続解除のため通信切断")
+            for(const [i,t] of ts.entries()){
+                clearTimeout(t)
+                console.log(`t[${i}]: 削除完了`)
+            }
+        }
+    }, [])
     return state
 }
 
