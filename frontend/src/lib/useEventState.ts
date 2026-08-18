a7a0bb7db32be3746ac59b3f174b2755d656e3c2
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-19 04:49:13 +0900

    接続先のURLを引数に追加

--- frontend/src/lib/useEventState.ts
+++ frontend/src/lib/useEventState.ts
@@ -6,9 +6,14 @@ import { adminWaiting, adminQuestionFour, adminQuestionArunashi, adminAnswerAri,
 import { monitorWaiting, monitorQuestionFour, monitorQuestionArunashi, monitorAnswerAri, monitorAnswerNashi, monitorFinished, } from "./mock/monitor/index"
 import { phoneWaiting, phoneQuestionFour, phoneQuestionArunashi, phoneAnswerAri, phoneAnswerNashi, phoneFinished } from "./mock/phone/index"
 
-import { USE_MOCK } from "./config"
+import { BASE, USE_MOCK } from "./config"
 
-function useEventState<Type>(testSteps: {at:number, mock:Type}[]) :Type | null{
+type opts<Type> = {
+    path:string
+    testSteps:{at:number, mock:Type}[]
+}
+
+function useEventState<Type>({path,testSteps,}:opts<Type>) :Type | null{
     const [state, setState] = useState<Type | null>(null)
     useEffect(() =>{
         if(USE_MOCK){
@@ -24,31 +29,41 @@ function useEventState<Type>(testSteps: {at:number, mock:Type}[]) :Type | null{
                 }
             }
         }
+        const es = new EventSource(`${BASE}${path}`)
     }, [])
     return state
 }
 
-export const useAdminState = () => useEventState<AdminState>([
-    { at:   300, mock: adminWaiting          },
-    { at:  2000, mock: adminQuestionFour     },
-    { at:  5000, mock: adminAnswerAri        },
-    { at:  8000, mock: adminQuestionArunashi },
-    { at: 11000, mock: adminAnswerNashi      },
-    { at: 14000, mock: adminFinished         },
-])
-export const useMonitorState = () => useEventState<MonitorState>([
-    { at:   300, mock: monitorWaiting          },
-    { at:  2000, mock: monitorQuestionFour     },
-    { at:  5000, mock: monitorAnswerAri        },
-    { at:  8000, mock: monitorQuestionArunashi },
-    { at: 11000, mock: monitorAnswerNashi      },
-    { at: 14000, mock: monitorFinished         },
-])
-export const useViewerState = () => useEventState<ViewerState>([
-    { at:   300, mock: phoneWaiting          },
-    { at:  2000, mock: phoneQuestionFour     },
-    { at:  5000, mock: phoneAnswerAri        },
-    { at:  8000, mock: phoneQuestionArunashi },
-    { at: 11000, mock: phoneAnswerNashi      },
-    { at: 14000, mock: phoneFinished         },
-])
+export const useAdminState = () => useEventState<AdminState>({
+    path:`/api/admin/events?token=<ADMIN_TOKEN>`,
+    testSteps:[
+        { at:   300, mock: adminWaiting          },
+        { at:  2000, mock: adminQuestionFour     },
+        { at:  5000, mock: adminAnswerAri        },
+        { at:  8000, mock: adminQuestionArunashi },
+        { at: 11000, mock: adminAnswerNashi      },
+        { at: 14000, mock: adminFinished         },
+    ]
+})
+export const useMonitorState = () => useEventState<MonitorState>({
+    path:'/api/events?view=monitor',
+    testSteps:[
+        { at:   300, mock: monitorWaiting          },
+        { at:  2000, mock: monitorQuestionFour     },
+        { at:  5000, mock: monitorAnswerAri        },
+        { at:  8000, mock: monitorQuestionArunashi },
+        { at: 11000, mock: monitorAnswerNashi      },
+        { at: 14000, mock: monitorFinished         },
+    ]
+})
+export const useViewerState = () => useEventState<ViewerState>({
+    path:'/api/events?view=phone',
+    testSteps:[
+        { at:   300, mock: phoneWaiting          },
+        { at:  2000, mock: phoneQuestionFour     },
+        { at:  5000, mock: phoneAnswerAri        },
+        { at:  8000, mock: phoneQuestionArunashi },
+        { at: 11000, mock: phoneAnswerNashi      },
+        { at: 14000, mock: phoneFinished         },
+    ]
+})
