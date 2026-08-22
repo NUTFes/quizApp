88c9ca1b900897203f28fa8c4bc9fead0da1bb66
sanpayy <s263136@stn.nagaokaut.ac.jp>
2026-08-23 01:23:37 +0900

    fix: tailwindの自動整形

--- frontend/src/features/phone/WaitingView.tsx
+++ frontend/src/features/phone/WaitingView.tsx
@@ -2,26 +2,22 @@
 export function WaitingView() {
   return (
     <main className="min-h-dvh pt-[env(safe-area-inset-top)]">
-      <header className="h-15.5 flex items-center shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
+      <header className="flex h-15.5 items-center shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
         <div className="h-full w-15.5 px-3 py-4">
-          <img src="" alt="logo" className="justify-center h-full w-full rounded-[10px]" />
+          <img src="" alt="logo" className="h-full w-full justify-center rounded-[10px]" />
         </div>
-        <p className="text-[17px] font-bold justify-center py-2.5">
-          45th Quiz
-        </p>
+        <p className="justify-center py-2.5 text-[17px] font-bold">45th Quiz</p>
       </header>
-      <div className="px-5 pt-7 h-113 w-full">
-        <div className="h-full rounded-[28px] pt-8 px-8 space-y-7 shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
+      <div className="h-113 w-full px-5 pt-7">
+        <div className="h-full space-y-7 rounded-[28px] px-8 pt-8 shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
           <div className="pb-4">
-            <p className="flex justify-start text-left text-4xl pt-8">
+            <p className="flex justify-start pt-8 text-left text-4xl">
               まもなく
               <br />
               始まります
             </p>
           </div>
-          <p className="py-4 text-base">
-            会場中央へお集まりください
-          </p>
+          <p className="py-4 text-base">会場中央へお集まりください</p>
           <p className="py-4 text-sm">
             この画面はモニターと自動で同期します
             <br />
@@ -30,14 +26,12 @@ export function WaitingView() {
         </div>
       </div>
       <div className="px-4 pt-7 pb-2.5">
-        <div className="rounded-[20px] shadow-[0_6px_16px_0_rgba(25,32,133,0.08)] h-[153px] w-full flex items-center justift-start p-6 text-xl">
+        <div className="flex h-[153px] w-full items-center justify-start rounded-[20px] p-6 text-xl shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
           移動の際は走らないでください
         </div>
       </div>
-      <footer className="w-full h-full">
-        <div className="pt-8 px-2.5 text-center text-base">
-          第45回 技大祭実行委員会
-        </div>
+      <footer className="h-full w-full">
+        <div className="px-2.5 pt-8 text-center text-base">第45回 技大祭実行委員会</div>
       </footer>
     </main>
   )
