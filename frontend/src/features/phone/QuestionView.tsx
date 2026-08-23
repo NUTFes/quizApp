b2803cfd253637b95aef22250b5d2cfa6e1bec8e
sanpayy <s263136@stn.nagaokaut.ac.jp>
2026-08-24 03:14:17 +0900

    feat: 選択肢の作成

--- frontend/src/features/phone/QuestionView.tsx
+++ frontend/src/features/phone/QuestionView.tsx
@@ -10,17 +10,17 @@ type props = { state: ViewerState | null }
 export function QuestionView({ state }: props) {
   if (state === null) return null
   if (state.question === null) return null
-  
+
   let questionType
   switch (state.question.type) {
-    case "four_choice": 
-      questionType = "4択"
+    case 'four_choice':
+      questionType = '4択'
       break
-    case "two_choice":
-      questionType = "〇×"
+    case 'two_choice':
+      questionType = '〇×'
       break
-    case "arunashi":
-      questionType = "あるなし"
+    case 'arunashi':
+      questionType = 'あるなし'
       break
   }
 
@@ -31,20 +31,29 @@ export function QuestionView({ state }: props) {
           <img src="" alt="logo" className="flex h-full w-full justify-center rounded-[10px]" />
         </div>
         <p className="flex justify-center py-2.5 text-[17px] font-bold">45th Quiz</p>
-        <p className="flex ml-auto justify-center py-2.5 pr-4 text-[17px] font-bold">{ `${questionType}クイズ` }</p>
+        <p className="flex ml-auto justify-center py-2.5 pr-4 text-[17px] font-bold">{`${questionType}クイズ`}</p>
       </header>
-      <div>
-        <QuestionNumber count={state.askedCount} />
-        <QuestionText segments={state.question?.textSegments ?? []} />
-        <RemainingTime
-          serverTime={state.serverTime}
-          timeLimitSec={state.timeLimitSec}
-          questionStartedAt={state.questionStartedAt}
-        />
-        <ChoiceList
-          question={state.question}
-          correctChoiceId={state.answer?.correctChoiceId ?? null}
-        />
+      <div className="w-full">
+        <div className="rounded-[20px] mx-5 my-7 shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
+          <div className="flex h-[49px]">
+            <QuestionNumber count={state.askedCount} />
+            <p className="flex items-center justify-center ml-auto rounded-[20px] px-5 py-2 text-xs bg-amber-200 m-2">
+              回答受付中
+            </p>
+          </div>
+          <QuestionText segments={state.question?.textSegments ?? []} />
+          <RemainingTime
+            serverTime={state.serverTime}
+            timeLimitSec={state.timeLimitSec}
+            questionStartedAt={state.questionStartedAt}
+          />
+        </div>
+        <div className='flex justify-between px-5 w-full'>
+          <ChoiceList
+            question={state.question}
+            correctChoiceId={state.answer?.correctChoiceId ?? null}
+          />
+        </div>
       </div>
     </main>
   )
--- frontend/src/features/phone/parts/ChoiceList.tsx
+++ frontend/src/features/phone/parts/ChoiceList.tsx
@@ -11,13 +11,13 @@ export function ChoiceList({ question, correctChoiceId }: Props) {
 
   const layout =
     question.type === 'four_choice'
-      ? 'grid grid-cols-2' // 2×2
-      : 'grid grid-cols-2' // 左右（2つしか無いので同じ書き方で並ぶ）
+      ? 'grid grid-cols-2 w-full' // 2×2
+      : 'grid grid-cols-2 w-full' // 左右（2つしか無いので同じ書き方で並ぶ）
 
   return (
     <div className={layout}>
       {question.choices.map((c) => (
-        <ChoiceCard key={c.id} choice={c} isCorrect={c.id === correctChoiceId} />
+        <ChoiceCard key={c.id} choice={c} isCorrect={c.id === correctChoiceId} type={question.type} />
       ))}
     </div>
   )
