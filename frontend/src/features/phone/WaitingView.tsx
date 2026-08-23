46c5dc6473c4633284b30a9139a0af572b77637b
sanpayy <s263136@stn.nagaokaut.ac.jp>
2026-08-23 19:00:11 +0900

    feaxt: 終了、アンケート誘導画面の作成

--- frontend/src/features/phone/WaitingView.tsx
+++ frontend/src/features/phone/WaitingView.tsx
@@ -4,9 +4,9 @@ export function WaitingView() {
     <main className="min-h-dvh pt-[env(safe-area-inset-top)]">
       <header className="flex h-15.5 items-center shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
         <div className="h-full w-15.5 px-3 py-4">
-          <img src="" alt="logo" className="h-full w-full justify-center rounded-[10px]" />
+          <img src="" alt="logo" className="flex h-full w-full justify-center rounded-[10px]" />
         </div>
-        <p className="justify-center py-2.5 text-[17px] font-bold">45th Quiz</p>
+        <p className="flex justify-center py-2.5 text-[17px] font-bold">45th Quiz</p>
       </header>
       <div className="h-113 w-full px-5 pt-7">
         <div className="h-full space-y-7 rounded-[28px] px-8 pt-8 shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
