01d306af06af7b6465383d1db044e649e9759845
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-23 11:15:10 +0900

    ヘッダをそのままトークン直書きで比較する認証機能を作成

--- backend/internal/platform/auth.go
+++ backend/internal/platform/auth.go
@@ -2,14 +2,19 @@ package platform
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// ここでトークンの照合をする（現状は何も水ただ通す（c.Next()））
+// ここでトークンの照合をする（現状はトークン直書きでヘッダ丸ごと検証）
 func RequireToken() gin.HandlerFunc {
 	return func(c *gin.Context){
 		log.Println("called RequireToken()")
+		if c.GetHeader("Authorization") != "koreha-tesuto-youno-tookunn-desu" {
+			RespondError(c, http.StatusUnauthorized, "UNAUTHORIZED", "トークンが違います")
+			return
+		}
 		c.Next()
 	}
 }
