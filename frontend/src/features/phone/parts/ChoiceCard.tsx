81b7647bc68b7e09adb6650bfa0123d110e42635
sanpayy <s263136@stn.nagaokaut.ac.jp>
2026-08-24 03:14:17 +0900

    feat: 選択肢の作成

--- frontend/src/features/phone/parts/ChoiceCard.tsx
+++ frontend/src/features/phone/parts/ChoiceCard.tsx
@@ -1,9 +1,17 @@
 import type { Choice } from '../../../types'
 
-type Props = { choice: Choice; isCorrect?: boolean }
+type Props = { choice: Choice; isCorrect?: boolean; type: string | null}
 
-export function ChoiceCard({ choice, isCorrect = false }: Props) {
+export function ChoiceCard({ choice, isCorrect = false, type }: Props) {
+  const normalStyle = 'w-43.5 rounded-[18px] shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]'
+  const correctStyle = ''
+  const isRight = choice.id === 'B' || choice.id === 'D' ? 'ml-auto' : ''
+  const isTwo = type === 'four_choice' ? 'h-33' : 'h-69'
+
   return (
-    <div className={isCorrect ? '（正答のスタイル）' : '（通常のスタイル）'}>{choice.text}</div>
+    <div className={`${isCorrect ? correctStyle : normalStyle} ${isRight} ${isTwo}`}>
+      <p className='flex justify-center items-center text-[14px] h-9 w-9 rounded-[10px] m-3 bg-amber-300'>{choice.id}</p>
+      <p className='p-3 text-base text-left'>{choice.text}</p>
+    </div>
   )
 }
