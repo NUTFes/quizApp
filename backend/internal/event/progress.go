5dab8267aa3c5537cc26474e821be94ba289ee9d
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-24 09:01:57 +0900

    リクエスト取得のための型の定義

--- backend/internal/event/progress.go
+++ backend/internal/event/progress.go
@@ -7,6 +7,14 @@ import (
 
 // question フェーズへ移行する関数
 func showQuestion(c *gin.Context, db *gorm.DB){
+	// リクエスト を取得するためのリクエストの型を定義
+	var req struct {
+		QuestionId uint `json: "quesitonId"`
+		TimeLimitSec *int `json: "timeLimitSec"` // これは任意
+	}
+
+	// state テーブルの asked を書き換える
+
 	// 最後の処理として、getState で行うことをそのまま行うため、そのまま呼び出す
 	getState(c, db)
 }
