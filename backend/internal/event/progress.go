050d0b1039f1036fb46394794240fa3b0c945f22
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-24 09:03:03 +0900

    リクエストの形が構造的にかどうかのチェック

--- backend/internal/event/progress.go
+++ backend/internal/event/progress.go
@@ -1,7 +1,10 @@
 package event
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/naoto-anzai/quizApp/backend/internal/platform"
 	"gorm.io/gorm"
 )
 
@@ -12,6 +15,29 @@ func showQuestion(c *gin.Context, db *gorm.DB){
 		QuestionId uint `json: "quesitonId"`
 		TimeLimitSec *int `json: "timeLimitSec"` // これは任意
 	}
+
+	// 指定したリクエストの型でリクエストが来ているかのチェック
+	if c.ShouldBind(&req) != nil {
+		platform.RespondError(c, http.StatusBadRequest, "INVALID_REQUEST", "リクエストの形が不正です")
+		return
+	}
+
+	// questionId がリクエストにふくまれるか？（questionIdが含まれない場合、Go によるゼロ値代入で、 0 が入る）　０じゃないか？
+	if req.QuestionId == 0 {
+		platform.RespondError(c, http.StatusBadRequest, "INVALID_REQUEST", "questionId は必須です")
+		return
+	}
+
+	// timeLimitSec が正しい範囲内で設定されているか
+	timeLimitSec := 30 // timeLimitSec を省略するときは規定値 30 に設定
+	if req.TimeLimitSec != nil {
+		if *req.TimeLimitSec < 5 || *req.TimeLimitSec > 120 {
+			platform.RespondError(c, http.StatusBadRequest, "INVALID_REQUEST", "timiLimitSec は 5~120 で指定してください")
+			return
+		}
+		timeLimitSec = *req.TimeLimitSec
+	}
+
 
 	// state テーブルの asked を書き換える
 
