6a096c796d192564c88c4c6c73a8fc58adb37d5b
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-24 12:27:56 +0900

    mise run fix

--- backend/internal/event/state.go
+++ backend/internal/event/state.go
@@ -72,11 +72,10 @@ func buildState(es EventState, q *question.Question, askedCount int) State {
 	return s
 }
 
-
 // event_states テーブルから読み込む
 //
 // es に状態を書き込む
-func readEventState(c *gin.Context, db *gorm.DB, es *EventState) bool{
+func readEventState(c *gin.Context, db *gorm.DB, es *EventState) bool {
 	// まず、event_states テーブル側に問題がないかをチェック
 	if err := db.First(es, 1).Error; err != nil { // if 分の中でerr による分岐が入るため、一度err に入れる
 		if errors.Is(err, gorm.ErrRecordNotFound) {
@@ -89,4 +88,3 @@ func readEventState(c *gin.Context, db *gorm.DB, es *EventState) bool{
 	}
 	return true
 }
-
