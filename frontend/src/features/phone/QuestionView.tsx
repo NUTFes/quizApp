4ff6466bbcd1ffb094638d594dc40ec95fa4f058
sanpayy <s263136@stn.nagaokaut.ac.jp>
2026-08-24 22:43:15 +0900

    fix: あるなし問題にも対応

--- frontend/src/features/phone/QuestionView.tsx
+++ frontend/src/features/phone/QuestionView.tsx
@@ -1,4 +1,5 @@
 // 問題表示画面
+import { useRemainingTime } from '../../lib/useRemainingTime'
 import { ViewerState } from '../../types'
 import { ChoiceList } from './parts/ChoiceList'
 import { QuestionNumber } from './parts/QuestionNumber'
@@ -23,10 +24,19 @@ export function QuestionView({ state }: props) {
       questionType = 'あるなし'
       break
   }
-  const announceTextTop = '選んだ選択肢のエリアに'
-  const announceTextBottom = '移動してください'
-  const attentionText = '歩きスマホや走っての移動はご遠慮ください'
-  
+  // useRemainingTimeで問題終了かどうかの判定を行う
+  const isAccepting = useRemainingTime({
+    serverTime: state.serverTime,
+    timeLimitSec: state.timeLimitSec,
+    questionStartedAt: state.questionStartedAt,
+  })
+  const isAcceptText = isAccepting ? '回答受付中' : '回答締切'
+  const isAcceptStyle = isAccepting ? 'bg-amber-200' : 'bg-gray-300'
+  const announceTextTop = isAccepting ? '選んだ選択肢のエリアに' : '回答を締め切りました'
+  const announceTextBottom = isAccepting ? '移動してください' : 'その場でお待ちください'
+  const attentionText = isAccepting
+    ? '歩きスマホや走っての移動はご遠慮ください'
+    : '移動はご遠慮ください'
 
   return (
     <main className="min-h-dvh pt-[env(safe-area-inset-top)]">
@@ -35,14 +45,16 @@ export function QuestionView({ state }: props) {
           <img src="" alt="logo" className="flex h-full w-full justify-center rounded-[10px]" />
         </div>
         <p className="flex justify-center py-2.5 text-[17px] font-bold">45th Quiz</p>
-        <p className="flex ml-auto justify-center py-2.5 pr-4 text-[17px] font-bold">{`${questionType}クイズ`}</p>
+        <p className="ml-auto flex justify-center py-2.5 pr-4 text-[17px] font-bold">{`${questionType}クイズ`}</p>
       </header>
       <div className="w-full">
-        <div className="rounded-[20px] mx-5 my-7 shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
+        <div className="mx-5 my-7 rounded-[20px] shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
           <div className="flex h-[49px]">
             <QuestionNumber count={state.askedCount} />
-            <p className="flex items-center justify-center ml-auto rounded-[20px] px-5 py-2 text-xs bg-amber-200 m-2">
-              回答受付中
+            <p
+              className={`m-2 ml-auto flex items-center justify-center rounded-[20px] px-5 py-2 text-xs shadow-[0_6px_16px_0_rgba(25,32,133,0.08)] ${isAcceptStyle}`}
+            >
+              {isAcceptText}
             </p>
           </div>
           <QuestionText segments={state.question?.textSegments ?? []} />
@@ -52,15 +64,17 @@ export function QuestionView({ state }: props) {
             questionStartedAt={state.questionStartedAt}
           />
         </div>
-        <div className='flex justify-between px-5 w-full'>
+        <div className="flex w-full justify-between px-5">
           <ChoiceList
             question={state.question}
             correctChoiceId={state.answer?.correctChoiceId ?? null}
           />
         </div>
       </div>
-      <div className='flex justify-center items-center text-center text-2xl h-[153px] p-6 mx-4 mt-7 mb-2.5 rounded-[20px] shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]'>
-        {announceTextTop}<br />{announceTextBottom}
+      <div className="mx-4 mt-7 mb-2.5 flex h-[153px] items-center justify-center rounded-[20px] p-6 text-center text-2xl shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
+        {announceTextTop}
+        <br />
+        {announceTextBottom}
       </div>
       <footer className="h-full w-full">
         <div className="px-2.5 pt-8 text-center text-base">{attentionText}</div>
--- frontend/src/features/phone/parts/ChoiceCard.tsx
+++ frontend/src/features/phone/parts/ChoiceCard.tsx
@@ -1,6 +1,6 @@
 import type { Choice } from '../../../types'
 
-type Props = { choice: Choice; isCorrect?: boolean; type: string | null}
+type Props = { choice: Choice; isCorrect?: boolean; type: string | null }
 
 export function ChoiceCard({ choice, isCorrect = false, type }: Props) {
   const normalStyle = 'w-43.5 rounded-[18px] shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]'
@@ -8,10 +8,25 @@ export function ChoiceCard({ choice, isCorrect = false, type }: Props) {
   const isRight = choice.id === 'B' || choice.id === 'D' ? 'ml-auto' : ''
   const isTwo = type === 'four_choice' ? 'h-33' : 'h-69'
 
+  if (type === 'arunashi') {
+    return (
+      <div className={`${isCorrect ? correctStyle : normalStyle} ${isRight} ${isTwo}`}>
+        <p
+          className={`mt-12 flex items-center justify-center px-10 pt-10 pb-16 text-[40px] ${choice.id === 'A' ? 'text-red-600' : 'text-blue-500'}`}
+        >
+          {choice.id === 'A' ? 'ある' : 'なし'}
+        </p>
+        <p className="p-4 pb-7.5 text-center text-xs">{choice.text}</p>
+      </div>
+    )
+  }
+
   return (
     <div className={`${isCorrect ? correctStyle : normalStyle} ${isRight} ${isTwo}`}>
-      <p className='flex justify-center items-center text-[14px] h-9 w-9 rounded-[10px] m-3 bg-amber-300'>{choice.id}</p>
-      <p className='p-3 text-base text-left'>{choice.text}</p>
+      <p className="m-3 flex h-9 w-9 items-center justify-center rounded-[10px] bg-amber-300 text-[14px]">
+        {choice.id}
+      </p>
+      <p className="p-3 text-left text-base">{choice.text}</p>
     </div>
   )
 }
