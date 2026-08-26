fd1c1cab084a03365c09f5b7c05d8f1f35672ad7
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-26 20:25:25 +0900

    認証状態を判定する機能を実装

---
+++ frontend/src/features/admin/LoginView.tsx
@@ -0,0 +1,8 @@
+// ログイン（トークン入力）ページ
+export function LoginView() {
+  return (
+    <div>
+      <h1>ログインページ</h1>
+    </div>
+  )
+}
---
+++ frontend/src/features/admin/OperationPanel.tsx
@@ -0,0 +1,8 @@
+// 操作パネル
+export function OperationPanel() {
+  return (
+    <div>
+      <h1>操作画面</h1>
+    </div>
+  )
+}
