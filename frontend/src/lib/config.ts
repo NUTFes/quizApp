b273b2b87b45eca22f2874094b0ba5714f97ca3b
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-26 17:44:29 +0900

    トークン管理に必要な関数を追加

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -7,3 +7,7 @@ if (!BASE) {
 export const USE_MOCK = import.meta.env.VITE_USE_MOCK === 'true'
 // 毎回呼び出す度に実行されてほしいから、定数としては扱えないため、関数オブジェクトで定義
 export const getAdminToken = () => localStorage.getItem('adminToken') ?? ''
+// ローカルストレージにトークンを保存する関数
+export const setAdminToken = (token: string) => localStorage.setItem('adminToken', token)
+// ローカルストレージに保存されているトークンを削除する（実質的なログアウト）関数
+export const clearAdminToken = () => localStorage.removeItem('adminToken')
