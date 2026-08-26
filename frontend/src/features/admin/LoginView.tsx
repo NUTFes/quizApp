00119ec79ba790006630473d261954529d2ad5f5
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-27 00:00:11 +0900

    フォーム送信のときに通信できるように変更

--- frontend/src/features/admin/LoginView.tsx
+++ frontend/src/features/admin/LoginView.tsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { setAdminToken } from '../../lib/config'
 import { verify } from '../../lib/api'
 
 // ログイン（トークン入力）ページ
 export function LoginView() {
   const [token, setToken] = useState<string>('')
-  useEffect(() => {
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
     setAdminToken(token)
     verify()
-  }, [token])
+  }
   return (
     <div>
       <h1>ログインページ</h1>
-      <label>
+      <form onSubmit={handleSubmit}>
         トークン（ADMIN_TOKEN）:
         <input name="tokenForm" value={token} onChange={(e) => setToken(e.target.value)} />
-      </label>
+        <button type="submit">ログイン</button>
+      </form>
     </div>
   )
 }
