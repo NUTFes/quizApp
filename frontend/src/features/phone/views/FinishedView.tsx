88f5a171d9d206cf800462365782e50cef5eb555
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-28 06:52:55 +0900

    細かなデザイン修正

--- frontend/src/features/phone/views/FinishedView.tsx
+++ frontend/src/features/phone/views/FinishedView.tsx
@@ -5,8 +5,8 @@ export function FinishedView() {
   return (
     <PhoneLayout>
       <div className="w-full px-5 pt-7">
-        <div className="flex w-full flex-col items-center justify-center rounded-[28px] border border-border-soft bg-surface shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
-          <p className="flex justify-center px-8 pt-16 pb-17 text-left text-message-l">
+        <div className="flex w-full flex-col items-center justify-center rounded-[28px] bg-brand shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
+          <p className="flex justify-center px-8 pt-16 pb-17 text-surface text-left text-message-l">
             ご参加
             <br />
             ありがとうございました
@@ -16,7 +16,7 @@ export function FinishedView() {
       </div>
       <div className="w-full px-5 pt-20">
         <div className="w-full rounded-[20px] border border-border-soft bg-surface pb-4 shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
-          <p className="flex justify-center px-8 py-8 text-center text-instruction-alt-black">
+          <p className="flex justify-center px-8 py-8 text-brand text-center text-instruction-alt-black">
             アンケートにご協力ください
           </p>
           <a
