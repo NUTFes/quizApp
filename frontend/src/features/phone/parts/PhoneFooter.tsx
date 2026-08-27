b14f2378bae369e04871dd8bc14ece5b872913d4
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-28 05:30:29 +0900

    フッターのデザインの警備な修正

--- frontend/src/features/phone/parts/PhoneFooter.tsx
+++ frontend/src/features/phone/parts/PhoneFooter.tsx
@@ -5,7 +5,7 @@ type PhoneFooterProps = {
 export function PhoneFooter({ footMessage = '第45回 技大祭実行委員会' }: PhoneFooterProps) {
   return (
     <footer className="mt-auto w-full">
-      <div className="px-2.5 pt-8 text-center text-notes">{footMessage}</div>
+      <div className="px-2.5 pt-8 pb-6 text-center text-notes">{footMessage}</div>
     </footer>
   )
 }
