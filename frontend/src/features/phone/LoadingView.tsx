1256604cb76ac820b4c8fd8da299cd89ae1c40ac
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-27 15:55:41 +0900

    デザインのずれを修正

--- frontend/src/features/phone/LoadingView.tsx
+++ frontend/src/features/phone/LoadingView.tsx
@@ -13,8 +13,8 @@ export function LoadingView() {
         <p className="flex justify-center py-2.5 text-header">45th Quiz</p>
       </header>
       <div className="w-full px-5 pt-7">
-        <div className="flex h-full justify-center space-y-7 rounded-[28px] border border-border-soft bg-surface px-8 pt-8 shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
-          <p className="flex items-center px-8 py-4 text-left text-message-m">
+        <div className="flex h-full justify-center space-y-7 rounded-[28px] bg-brand px-8 pt-8 shadow-[0_6px_16px_0_rgba(25,32,133,0.08)]">
+          <p className="flex items-center px-8 py-4 text-left text-surface text-message-m">
             しばらくお待ちください
           </p>
         </div>
