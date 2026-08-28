6deec722c2c6b0856768f89790f81d91dd1e1c03
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-08-29 03:44:20 +0900

    Broadcaster のコンストラクタの定義

--- backend/internal/sse/broadcast.go
+++ backend/internal/sse/broadcast.go
@@ -1,7 +1,20 @@
+package sse
+
 type Broadcaster struct {
 	admin *Hub
 	monitor *Hub
 	phone *Hub
+}
+
+func NewBroadcaster() *Broadcaster{
+	admin := NewHub()
+	monitor := NewHub()
+	phone := NewHub()
+	return &Broadcaster{
+		admin,
+		monitor,
+		phone,
+	}
 }
 
 func (b *Broadcaster) BroadcastAll(adminJSON, monitorJSON, phoneJSON []byte){
