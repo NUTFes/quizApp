bff1d50369fd85edf81d91ad22cb43bc066a26f8
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-09-10 22:45:42 +0900

    feat: /images を proxy に追加

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,10 @@ export default defineConfig({
         target: 'http://backend:3000',
         changeOrigin: true,
       },
+      '/images': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      },
     },
   },
 })
