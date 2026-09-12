96db6e2262432464be0c51f3110ae5c322e2d5d7
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-09-12 23:50:50 +0900

    頻繁に使う情報をstate から分離する

--- frontend/src/features/admin/parts/CurrentStatus.tsx
+++ frontend/src/features/admin/parts/CurrentStatus.tsx
@@ -7,10 +7,16 @@ type Props = {
 }
 
 export function CurrentStatus({ state, remainingTime }: Props) {
-  return (
-    <section>
-      <h2>出題中の問題</h2>
-      <p>{phaseLabel(state.phase)}</p> {/*待機・終了*/}
-    </section>
-  )
+  const question = state.question
+  if (question === null) {
+    return (
+      <section>
+        <h2>出題中の問題</h2>
+        <p>{phaseLabel(state.phase)}</p> {/*待機・終了*/}
+      </section>
+    )
+  }
+
+  const correct = question.choices.find((choice) => choice.id === question.correctChoiceId)
+  return <div></div>
 }
