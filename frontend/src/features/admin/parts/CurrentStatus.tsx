ceaad9290812aec7a3b0faa52af350e86d17b09b
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-09-12 22:30:18 +0900

    現在出題中の問題の詳細情報を表示するパーツの仮実装

---
+++ frontend/src/features/admin/parts/CurrentStatus.tsx
@@ -0,0 +1,10 @@
+import { AdminState } from '../../../types'
+
+type Props = {
+  state: AdminState
+  remainingTime: number
+}
+
+export function CurrentStatus({ state, remainingTime }: Props) {
+  return <div></div>
+}
