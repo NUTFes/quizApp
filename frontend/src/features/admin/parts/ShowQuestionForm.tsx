79c4e79448f87674e71fcc24bb273a0060b33655
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-09-14 03:33:18 +0900

    最大、最小値の定義と、Props の定義

---
+++ frontend/src/features/admin/parts/ShowQuestionForm.tsx
@@ -0,0 +1,13 @@
+import { QuestionListItem } from '../../../types'
+
+const MIN_SEC = 5
+const MAX_SEC = 120
+
+type Props = {
+  selected: QuestionListItem | null // 未選択は null
+  currentQuestionId: number | null // 今出している問題のid 今何も出してないなら null
+  timeLimitInput: string // 入力欄の文字列
+  busy: boolean
+  onTimeLimitInputChange: (value: string) => void
+  onSubmit: (questionId: number, timeLimitSec: number) => void
+}
