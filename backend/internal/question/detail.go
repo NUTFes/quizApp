ade5b79d0b944280de91afff002429832ef709ec
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-09-13 09:50:51 +0900

    細かい修正

--- backend/internal/question/detail.go
+++ backend/internal/question/detail.go
@@ -1,4 +1,4 @@
-// GET /api/admin/question/:id(問題の詳細情報)のハンドラ
+// GET /api/admin/questions/:id(問題の詳細情報)のハンドラ
 package question
 
 import (
@@ -16,7 +16,7 @@ func getQuestion(c *gin.Context, db *gorm.DB) {
 
 	id, err := strconv.ParseUint(raw, 10, 64) // 文字列を数値（idようにuint）に変換
 	if err != nil {
-		// 数値以外がもともと来ていても、id に対する問題ｇ存在しないというエラーにする
+		// 数値以外がもともと来ていても、id に対する問題が存在しないというエラーにする
 		platform.RespondError(c, http.StatusNotFound, "QUESTION_NOT_FOUND", "questionId="+raw+" は存在しません")
 		return
 	}
@@ -25,7 +25,7 @@ func getQuestion(c *gin.Context, db *gorm.DB) {
 	// エラー内容で切り分ける
 	if err := db.First(&q, uint(id)).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			platform.RespondError(c, http.StatusNotFound, "QUESTION_NOT_FOUND", "questionId="+raw+"は存在しません")
+			platform.RespondError(c, http.StatusNotFound, "QUESTION_NOT_FOUND", "questionId="+raw+" は存在しません")
 			return
 		}
 		platform.RespondError(c, http.StatusInternalServerError, "INTERNAL", "問題データを読み込めませんでした")
