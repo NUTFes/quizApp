b1dfe67f7d77c9b4abf4bd3f4d8acd0c2563fcae
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-09-13 09:20:04 +0900

    概念的なここでやる事を記述

--- backend/internal/question/detail.go
+++ backend/internal/question/detail.go
@@ -1,4 +1,16 @@
 // GET /api/admin/question/:id(問題の詳細情報)のハンドラ
 package question
 
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+
+func getQuestion(c *gin.Context, db *gorm.DB) {
+	raw := c.Param("id") // :id の部分に来る数値をid 変数として持っておく
+	var q Question
+	c.JSON(http.StatusOK, q)
+}
